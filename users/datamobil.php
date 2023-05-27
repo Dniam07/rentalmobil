a007dc325124b041abfaecd76f9a75d7e084c0bb
Dniam07 <gamingopan123@gmail.com>
2023-05-27 19:30:35 +0700

    bantu nih

--- users/datamobil.php
+++ users/datamobil.php
@@ -48,13 +48,12 @@ if (isset($_GET['id_mobil'])) {
                 <nav class="nav_container">
 
                     <ul class="nav_wrapper">
-                        <li><a href="" class="dropdown-menu">menu</a></li>
-                        <ul class="dropdown-content">
-                            <li><a href="datamobil.php">data mobil</a></li>
-                            <li><a href="datapenyewa.php">data penyewa</a></li>
-                            <li><a href="datatransaksi.php">data transaksi</a></li>
-                            <li><a href="home.php">home</a></li>
-                        </ul>
+                        <li><a href="datamobil.php">data mobil</a></li>
+                        <li><a href="datapenyewa.php">data penyewa</a></li>
+                        <li><a href="datatransaksi.php">data transaksi</a></li>
+                        <li><a href="datatransaksi.php">konfirmasi pesanan</a></li>
+                        <li><a href="home.php" class="nav-home">logout</a></li>
+                    </ul>
                     </ul>
                 </nav>
             </div>
@@ -63,86 +62,91 @@ if (isset($_GET['id_mobil'])) {
 
 
     <br><br><br><br><br><br><br><br>
+
     <div class="container">
-        <div class="form-title">
-            <h1 class="h1-form-title">INSERT FORM</h1>
-        </div>
-        <div class="form-insert">
-            <form action="" method="post">
-                <input type="hidden" name="id" id="id_buku">
-                <ul>
+        <section class="form-wrapper">
 
-                    <li>
-                        <label for="jenis_mobil">Jenis Mobil :</label>
-                        <input type="text" name="jenis_mobil" id="jenis_mobil" autocomplete="off"
-                            placeholder="Masukkan Jenis Mobil">
-                    </li>
+            <div class="form-title">
+                <h1 class="h1-form-title">TAMBAH DATA MOBIL</h1>
+            </div>
+            <div class="form-insert">
+                <form action="" method="post">
+                    <input type="hidden" name="id" id="id_buku">
+                    <ul class="form-mobil">
 
-                    <li>
-                        <label for="nama_mobil">Nama Mobil :</label>
-                        <input type="text" name="nama_mobil" id="nama_mobil" autocomplete="off"
-                            placeholder="Masukkan Nama Mobil">
-                    </li>
+                        <li>
+                            <label for="jenis_mobil">Jenis Mobil :</label><br>
+                            <input type="text" name="jenis_mobil" id="jenis_mobil" autocomplete="off" placeholder="Masukkan Jenis Mobil">
+                        </li>
 
-                    <li>
-                        <label for="harga_harian">Harga Harian :</label>
-                        <input type="text" name="harga_harian" id="harga_harian" autocomplete="off"
-                            placeholder="Masukkan Harga Harian">
-                    </li>
+                        <li>
+                            <label for="nama_mobil">Nama Mobil :</label><br>
+                            <input type="text" name="nama_mobil" id="nama_mobil" autocomplete="off" placeholder="Masukkan Nama Mobil">
+                        </li>
 
-
-                </ul>
-                <button type="submit" name="submit-mobil">kirim</button>
-
-            </form>
-
-        </div>
-
-        <div class="judul-tabel">
-            <h1>TABEL MOBIL</h1>
-        </div>
+                        <li>
+                            <label for="harga_harian">Harga Harian :</label><br>
+                            <input type="text" name="harga_harian" id="harga_harian" autocomplete="off" placeholder="Masukkan Harga Harian">
+                        </li>
 
 
-        <table>
-            <tr>
-                <th>No.</th>
-                <th>Jenis Mobil</th>
-                <th>Nama Mobil</th>
-                <th>Harga Harian</th>
-                <th>Aksi</th>
-            </tr>
+                    </ul>
+                    <button type="submit" name="submit-mobil" class="submit-form" >Tambah Data</button>
 
-            <?php
-            $i = 1;
-            ?>
-            <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                </form>
+
+            </div>
+        </section>
+        <section class="car-table">
+
+            <div class="table-title">
+                <h1>TABEL MOBIL</h1>
+            </div>
+
+
+            <table class="table-data">
                 <tr>
-                    <td>
-                        <?php echo $i++; ?>
-                    </td>
-                    <td>
-                        <?php echo $row['jenis_mobil'] ?>
-                    </td>
-                    <td>
-                        <?php echo $row['nama_mobil'] ?>
-                    </td>
-                    <td>
-                        <?php echo $row['harga_harian'] ?>
-                    </td>
-
-                    <td>
-                        <a href="edit.php?id=<?php echo $row['id_mobil'] ?>">Edit</a>
-                        <a href="?id_mobil=<?php echo $row['id_mobil'] ?>">Hapus</a>
-                    </td>
+                    <th>No.</th>
+                    <th>Jenis Mobil</th>
+                    <th>Nama Mobil</th>
+                    <th>Harga Harian</th>
+                    <th>Aksi</th>
                 </tr>
-            <?php endwhile ?>
-        </table>
 
-        <div class="kembali">
-            <p><a href="home.php">KEMBALI</a></p>
-        </div>
+                <?php
+                $i = 1;
+                ?>
+                <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                    <tr>
+                        <td>
+                            <?php echo $i++; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['jenis_mobil'] ?>
+                        </td>
+                        <td>
+                            <?php echo $row['nama_mobil'] ?>
+                        </td>
+                        <td>
+                            <?php echo $row['harga_harian'] ?>
+                        </td>
+
+                        <td>
+                            <a href="edit.php?id=<?php echo $row['id_mobil'] ?>">Edit</a>
+                            <a href="?id_mobil=<?php echo $row['id_mobil'] ?>">Hapus</a>
+                        </td>
+                    </tr>
+                <?php endwhile ?>
+            </table>
+
+            <div class="kembali">
+                <p><a href="home.php">KEMBALI</a></p>
+            </div>
+        </section>
 
     </div>
+
+
 
 
 
